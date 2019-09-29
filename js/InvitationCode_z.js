5f683a8f0fb91e726a5e4658dca85cfe8da1842b
zzcneusoft <1253226091@qq.com>
2019-09-29 20:52:40 +0800

    zzc

--- js/InvitationCode_z.js
+++ js/InvitationCode_z.js
@@ -50,7 +50,7 @@
 		layui.use('form', function(){
 			  var form = layui.form; //只有执行了这一步，部分表单元素才会自动修饰成功
 	
-			//艺术门类
+			//年份
 					var data = {
 					}
 					base.commonAjax('clav/allotInfo/getYearList', data, function (data) {
@@ -61,6 +61,9 @@
 											$('#nianfen').append(
 													'<option value="' + data.data[i] + '">' + data.data[i] + '</option>'
 											);
+											$('#nianfen2').append(
+												'<option value="' + data.data[i] + '">' + data.data[i] + '</option>'
+										);
 									}
 									form.render();
 							}
@@ -74,9 +77,10 @@ function codelist_z() {
 	}
 	base.commonAjax('clav/allotInfo/getAllotInfoList', data, function (data) {
 		var pagedata = [];
-		console.log("sd",data.totalCount);
+		//console.log("sd",data.totalCount);
 		pagedata = data.data;
 		console.log(data)
+		$('#table_count').empty();
 		$('#table_count').append(
 			'发展名额数(共计'+data.totalCount+'个)'
 		)
@@ -106,7 +110,7 @@ function codelist_z() {
 		});
 	});
 }
-
+codelist_z();
 function auto(index, obj) {
 	return '<tr>'+
 			'<td>' + (index + 1) + '</td>'+
@@ -116,9 +120,143 @@ function auto(index, obj) {
 			'<td>' + obj.placeCount + '</td>'+
 			'<td>' + (obj.fenpeiTime).split(/\s+/)[0] + '</td>'+
 			'<td>'+
-				'<button class="layui-btn">修改</button>'+
-				'<button class="layui-btn layui-btn-danger">删除</button>'+
+				'<button id="update'+ obj.id+'"class="layui-btn update_z" onclick=update_z('+obj.id+')>修改</button>'+
+				'<button style="margin-left: 7px;" id="remove'+ obj.id+'"class="layui-btn layui-btn-danger update_mv" onclick=remove_z('+obj.id+')>删除</button>'+
 			'</td>'+
 		'</tr>'
 }
-codelist_z();
+//分配列表的修改
+function update_z(nid){
+   //console.log($(this).attr('id').replace("find",""));
+   $(".cover5").css("display", "inline-block");
+    fpid=nid;
+   var data={
+	   id:fpid
+   }
+   base.commonAjax('clav/allotInfo/getAllotInfoList', data, function (data) {
+		   console.log(data.data[0].placeCount)
+		   $('#fzmes_z').val(data.data[0].placeCount)
+   })
+}
+function fzmesqd_z(){
+	var newfid=$('#fzmes_z').val()
+	var data={
+		id:fpid,
+		placeCount:newfid
+	}
+	base.commonAjax('clav/allotInfo/updateAllotInfo', data, function (data) {
+			alert('修改成功')
+			$(".cover5").css( "display", "none");
+			codelist_z();
+	})
+}
+//分配列表的删除
+function remove_z(nid){
+	var reid=nid;
+	var data={
+        id:reid
+    }
+    base.commonAjax('clav/allotInfo/deleteAllotInfo', data, function (data) {
+			alert('删除成功')
+			codelist_z();
+    })
+}
+////////////////////////////////////////////////////////////////////////
+//邀请码生产
+function codemake_z() {
+	var data = {
+
+	}
+	base.commonAjax('clav/allotInfo/getAllotInfoList', data, function (data) {
+		var pagedata = [];
+		//console.log("sd",data.totalCount);
+		pagedata = data.data;
+		//调用分页
+		layui.use(['laypage', 'layer'], function () {
+			var laypage = layui.laypage
+				, layer = layui.layer;
+
+			laypage.render({
+				elem: 'shuju2'
+				, count: pagedata.length,
+				layout: ['count', 'prev', 'page', 'next', 'limit', 'skip'],
+				limit: 10,
+				jump: function (obj) {
+					//模拟渲染
+					document.getElementById('sccode_z').innerHTML = function () {
+						var arr = []
+							, thisData = pagedata.concat().splice(obj.curr * obj.limit - obj.limit, obj.limit);
+						layui.each(thisData, function (index, item) {
+							arr.push(auto2(index, item));
+							//console.log(item);
+						});
+						return arr.join('');
+					}();
+				}
+			});
+		});
+	});
+}
+codemake_z();
+function auto2(index, obj) {
+	return '<tr>'+
+			'<td style="width: 50px">'+
+				'<input type="checkbox" name="" title="" lay-skin="primary">'+
+			'</td>'+
+			'<td>' + (index + 1) + '</td>'+
+			'<td>' + obj.tjType + '</td>'+
+			'<td>' + obj.dwType + '</td>'+
+			'<td>' + obj.dwName + '</td>'+
+			'<td style="color: skyblue" onclick="lookinfo()">查看详情</td>'+
+		'</tr>'
+}
+////////////////////////////////////////////////////////////////////////
+//邀请码生产
+function zzcode_z() {
+	var data = {
+
+	}
+	base.commonAjax('clav/allotInfo/getAllotInfoList', data, function (data) {
+		var pagedata = [];
+		//console.log("sd",data.totalCount);
+		pagedata = data.data;
+		//调用分页
+		layui.use(['laypage', 'layer'], function () {
+			var laypage = layui.laypage
+				, layer = layui.layer;
+
+			laypage.render({
+				elem: 'shuju3'
+				, count: pagedata.length,
+				layout: ['count', 'prev', 'page', 'next', 'limit', 'skip'],
+				limit: 10,
+				jump: function (obj) {
+					//模拟渲染
+					document.getElementById('zzyqmsq_z').innerHTML = function () {
+						var arr = []
+							, thisData = pagedata.concat().splice(obj.curr * obj.limit - obj.limit, obj.limit);
+						layui.each(thisData, function (index, item) {
+							arr.push(auto3(index, item));
+							//console.log(item);
+						});
+						return arr.join('');
+					}();
+				}
+			});
+		});
+	});
+}
+zzcode_z();
+function auto3(index, obj) {
+	return '<tr>'+
+			'<td>' + (index + 1) + '</td>'+
+			'<td>' + obj.fenpeiTime + '</td>'+
+			'<td>' + obj.dwName + '</td>'+
+			'<td>' + obj.placeCount + '</td>'+
+			'<td style="color: skyblue">以申领</td>'+
+			'<td>'+
+				'<button type="button" class="layui-btn layui-btn-normal">查看</button>'+
+				'<button style="margin-left: 7px;" type="button" class="layui-btn layui-btn-warm">导出</button>'+
+			'</td>'+
+		'</tr>'
+}
